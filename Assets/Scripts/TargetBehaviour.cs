6e72a9a577546769d6a98e98b72c4a4da5da3a2f
Cagema <Cagema.erm@gmail.com>
2023-08-06 16:43:37 +0300

    First commit

---
+++ Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetBehaviour : MonoBehaviour
+{
+    [SerializeField] string[] _tags;
+    [SerializeField] Sprite[] _sprites;
+
+    private void Start()
+    {
+        int randIndex = Random.Range(0, _tags.Length);
+        tag = _tags[randIndex];
+        GetComponent<SpriteRenderer>().sprite = _sprites[randIndex];
+    }
+    private void Update()
+    {
+        if (GameManager.Single.GameActive)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Vector2.zero, Time.deltaTime * GameManager.Single.Speed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(tag))
+        {
+            GameManager.Single.Score++;
+            Destroy(gameObject);
+        }
+        else if (collision.CompareTag("Player"))
+        {
+            GameManager.Single.LostLive();
+        }
+    }
+}
